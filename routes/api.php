2ad037fcb6a6a28fa50dc4e9b1ed2404062d262f
GummyBear0200 <malacastec123@gmail.com>
2025-11-11 23:20:15 +0800

    fixed laragon independencies and started authentication framework via laravel

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\AuthenticationController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,4 +24,5 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::post('/add-user', [UserController::class, 'addUser']);
     Route::put('/edit-user/{id}', [UserController::class, 'editUser']);
     Route::delete('/delete-user/{id}', [UserController::class, 'deleteUser']);
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
 });
